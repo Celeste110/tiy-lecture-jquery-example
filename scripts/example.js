cb437572f8a862dcf922cbcf09e114e25368f2bb
Doug Hughes <doug@theironyard.com>
2016-09-08 11:34:09 -0400

    stuff

--- scripts/example.js
+++ scripts/example.js
@@ -1,5 +1,17 @@
 $(function () {
 
+    $("#thing").keydown(function(){
+        $(this).removeClass("error");
+    });
+
+    $("#addThingButton").mouseover(function(){
+        $(this).css("visibility", "hidden");
+    });
+
+    $("#addThingButton").mouseout(function(){
+        $(this).css("visibility", "visible");
+    });
+
     $("#addThingButton").click(function () {
         addThing();
     });
@@ -22,40 +34,44 @@ function showCountDown(countDown){
 function addThing(){
     var thing = $("#thing").val().trim();
 
-    //$("#counter").text("3").delay(1000).text("2");
-    var countDown = 3;
-    showCountDown(countDown);
-    var timer = setInterval(function(){
-        countDown--;
+    if(thing) {
+        //$("#counter").text("3").delay(1000).text("2");
+        var countDown = 3;
         showCountDown(countDown);
+        var timer = setInterval(function () {
+            countDown--;
+            showCountDown(countDown);
 
-        if(countDown == 0){
-            actuallyDoTheAddThingBit(thing);
-            clearInterval(timer);
-        }
-    }, 1000);
+            if (countDown == 0) {
+                actuallyDoTheAddThingBit(thing);
+                clearInterval(timer);
+            }
+        }, 1000);
+    } else {
+        $("#thing").addClass("error");
+    }
 
 }
 
 function actuallyDoTheAddThingBit(thing){
-    if(thing) {
-        var a = $("<a href='javascript://'>X</a>");
-        a.click(function(){
-            $(this).parent().fadeOut(2000, function(){
-                $(this).remove();
-            });
-            //$(this).parent().remove();
+
+    var a = $("<a href='javascript://'>X</a>");
+    a.click(function(){
+        $(this).parent().fadeOut(2000, function(){
+            $(this).remove();
         });
+        //$(this).parent().remove();
+    });
 
-        var li = $("<li>" + thing + " </li>");
+    var li = $("<li>" + thing + " </li>");
 
-        li.append(a);
+    li.append(a);
 
-        $("ul").append(li);
+    $("ul").append(li);
 
-        $("#thing").val("");
-        $("#thing").focus();
+    $("#thing").val("");
+    $("#thing").focus();
 
-        $("#counter").text("");
-    }
+    $("#counter").text("");
+
 }
